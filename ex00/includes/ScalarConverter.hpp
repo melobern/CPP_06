0b9b2ac7ef585cd4720c3ffe23a58da95b30e933
mbernard <mbernard@student.42lyon.fr>
2024-09-24 18:06:54 +0200

    ex00 : it seems to work fine

--- ex00/includes/ScalarConverter.hpp
+++ ex00/includes/ScalarConverter.hpp
@@ -2,7 +2,7 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.hpp                                :+:      :+:    :+:   */
-/*                                                    +:+ +:+   cat         +:+     */
+/*                                                    +:+ +:+         +:+     */
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 12:48:17 by mbernard          #+#    #+#             */
@@ -54,6 +54,7 @@ class ScalarConverter {
     class InvalidInputException : public std::exception {
       virtual const char* what() const throw();
     };
+
  private:
   ScalarConverter(void);
   ScalarConverter(const ScalarConverter &src);
@@ -75,30 +76,3 @@ class ScalarConverter {
 std::ostream &operator<<(std::ostream &os, ScalarConverter::Scalar const &sc);
 
 #endif  //  EX00_INCLUDES_SCALARCONVERTER_HPP_
-// Write a class ScalarConverter that will contain only one static method "convert"
-// that will takes as parameter a string representation of a C++ literal in its most common
-// form and output its value in the following serie of scalar types :
-// • char
-// • int
-// • float
-// • double
-// As this class doesnt need to store anything at all, this class must not be instanciable by
-// users.
-// Except for char parameters, only the decimal notation will be used.
-// Examples of char literals: ’c’, ’a’, ...
-// To make things simple, please note that non displayable characters shouldnt be used as
-// inputs. If a conversion to char is not displayable, prints an informative message.
-// Examples of int literals: 0, -42, 42...
-// Examples of float literals: 0.0f, -4.2f, 4.2f...
-// You have to handle these pseudo literals as well (you know, for science): -inff, +inff
-// and nanf.
-// Examples of double literals: 0.0, -4.2, 4.2...
-// You have to handle these pseudo literals as well (you know, for fun): -inf, +inf and nan.
-//
-// Write a program to test that your class works as expected.
-// You have to first detect the type of the literal passed as parameter, convert it from
-// string to its actual type, then convert it explicitly to the three other data types. Lastly,
-// display the results as shown below.
-// If a conversion does not make any sense or overflows, display a message to inform
-// the user that the type conversion is impossible. Include any header you need in order to
-// handle numeric limits and special values.
